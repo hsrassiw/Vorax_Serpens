6384716969616f9ed620c8c156f2007b4a8214d3
hsrassiw <minhnghianshd@gmail.com>
2025-03-14 02:19:37 +0700

    update

--- cmake-build-debug/src/main.cpp
+++ cmake-build-debug/src/main.cpp
@@ -7,23 +7,45 @@
 #include "Renderer.hpp"
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0 || TTF_Init() == -1) {
-        std::cerr << "Initialization failed: " << SDL_GetError() << std::endl;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
-
-    std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> window(
-        SDL_CreateWindow("Vorax_Serpens", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                         Config::SCREEN_WIDTH, Config::SCREEN_HEIGHT, SDL_WINDOW_SHOWN),
-        SDL_DestroyWindow
-    );
-
+    SDL_Window* window = SDL_CreateWindow("Vorax Serpens",
+                                          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          Config::SCREEN_WIDTH, Config::SCREEN_HEIGHT,
+                                          SDL_WINDOW_SHOWN);
     if (!window) {
-        std::cerr << "Window creation failed: " << SDL_GetError() << std::endl;
-        TTF_Quit();
+        std::cerr << "Window Creation Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Renderer Creation Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) quit = true;
+        }
+
+        SDL_SetRenderDrawColor(renderer, Config::BACKGROUND_COLOR.r, Config::BACKGROUND_COLOR.g,
+                               Config::BACKGROUND_COLOR.b, Config::BACKGROUND_COLOR.a);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
